6f2dc2410a34df3283230f3a05a4ba799f99cb89
Zhu Rengen <cloverzrg@gmail.com>
2020-05-12 01:17:21 +0800

    Update logger.go

--- logger/logger.go
+++ logger/logger.go
@@ -77,6 +77,7 @@ func init() {
 	logger.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp:   true,
 		TimestampFormat: "2006-01-02 15:04:05.000",
+		ForceColors: true,
 	})
 	logger.SetReportCaller(false)
 
